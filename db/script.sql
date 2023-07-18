52f59bea4dd39d82a15bc3672f23af64e06a8455
Simone Maccari <s.maccari@geosystems.it>
2023-07-18 14:07:23 +0200

    corretto script sql

--- db/script.sql
+++ db/script.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
   `name` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
+  `username` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`username`)
 );
